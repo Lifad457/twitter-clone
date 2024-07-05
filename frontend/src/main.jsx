79be84012c2d9e58149f1311cbe4b6413d2e6f49
Cédric Bison <cedric.bison@gmail.com>
2024-07-05 07:42:19 +0200

    setting up front

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import {
+	Route,
+	RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+} from 'react-router-dom';
+import './index.css';
+import HomePage from './pages/home/HomePage.jsx';
+import SignUpPage from './pages/auth/signup/SignUpPage.jsx';
+import LoginPage from './pages/auth/login/LoginPage.jsx';
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path='/' element={<App />}>
+			<Route index element={<HomePage />} />
+			<Route path='/signup' element={<SignUpPage />} />
+			<Route path='/login' element={<LoginPage />} />
+		</Route>
+	)
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
