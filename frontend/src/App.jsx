b639926102b9c66121871d0d6d8c920f4661c76c
Cédric Bison <cedric.bison@gmail.com>
2024-07-12 07:17:45 +0200

    react-query i and signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,21 @@ import { Outlet } from 'react-router-dom';
 import Sidebar from './components/common/Sidebar';
 import RightPanel from './components/common/RightPanel';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from 'react-hot-toast';
+
 function App() {
+	const queryClient = new QueryClient();
+
 	return (
-		<div className='flex max-w-6xl mx-auto'>
-			<Sidebar />
-			<Outlet />
-			<RightPanel />
-		</div>
+		<QueryClientProvider client={queryClient}>
+			<div className='flex max-w-6xl mx-auto'>
+				<Sidebar />
+				<Outlet />
+				<RightPanel />
+				<Toaster />
+			</div>
+		</QueryClientProvider>
 	);
 }
 
