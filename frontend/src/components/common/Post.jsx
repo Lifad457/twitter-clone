4b89367954341b6f9aa6181b54e8c77542f3dc45
Cédric Bison <cedric.bison@gmail.com>
2024-07-17 07:26:52 +0200

    add date formating to posts

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -9,11 +9,17 @@ import PropTypes from 'prop-types';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
 import LoadingSpinner from './LoadingSpinner';
+import { formatPostDate } from '../../utils/date';
 
 const Post = ({ post }) => {
 	const [comment, setComment] = useState('');
 	const { data: authUser } = useQuery({ queryKey: ['authUser'] });
 	const queryClient = useQueryClient();
+	const postOwner = post.user;
+	const isLiked = post.likes.includes(authUser?._id);
+	const isMyPost = authUser?._id === postOwner._id;
+
+	const formattedDate = formatPostDate(post.createdAt);
 
 	const { mutate: deletePostMutation, isPending: isDeleting } = useMutation({
 		mutationFn: async () => {
@@ -70,14 +76,42 @@ const Post = ({ post }) => {
 		},
 	});
 
-	const postOwner = post.user;
-	const isLiked = post.likes.includes(authUser?._id);
+	const { mutate: commentOnPostMutation, isPending: isCommenting } =
+		useMutation({
+			mutationFn: async () => {
+				try {
+					const response = await fetch(
+						`/api/posts/comment/${post._id}`,
+						{
+							method: 'POST',
+							headers: {
+								'Content-Type': 'application/json',
+							},
+							body: JSON.stringify({
+								text: comment,
+							}),
+						}
+					);
 
-	const isMyPost = authUser?._id === postOwner._id;
-
-	const formattedDate = '1h';
-
-	const isCommenting = false;
+					const data = await response.json();
+					if (!response.ok)
+						throw new Error(
+							data.message || 'Failed to comment on post'
+						);
+					return data;
+				} catch (error) {
+					console.error(error);
+					throw error;
+				}
+			},
+			onSuccess: () => {
+				setComment('');
+				queryClient.invalidateQueries({ queryKey: ['posts'] });
+			},
+			onError: (error) => {
+				toast.error(error.message);
+			},
+		});
 
 	const handleDeletePost = () => {
 		deletePostMutation();
@@ -85,6 +119,8 @@ const Post = ({ post }) => {
 
 	const handlePostComment = (e) => {
 		e.preventDefault();
+		if (isCommenting) return;
+		commentOnPostMutation();
 	};
 
 	const handleLikePost = () => {
